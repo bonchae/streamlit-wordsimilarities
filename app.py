397d54fc139e60ee27331440cbeebacc90ca92b1
bonchae <bchae@ksu.edu>
2021-01-19 22:17:49 -0600

    first

--- app.py
+++ app.py
@@ -1,17 +1,37 @@
 # https://towardsdatascience.com/streamlit-and-spacy-create-an-app-to-predict-sentiment-and-word-similarities-with-minimal-domain-14085085a5d4
 
 import spacy
-from spacytextblob.spacytextblob import SpacyTextBlob
-import streamlit as st
+import streamlit as st 
+from itertools import combinations 
+import pandas as pd 
 
-nlp = spacy.load('en_core_web_sm')
-spacy_text_blob = SpacyTextBlob()
-nlp.add_pipe(spacy_text_blob)
-text = 'Today is an amazing day!'
 
-st.title('Sentiment app')
-user_input = st.text_input("Text", text)
-doc = nlp(user_input)
+st.title('Similarity app')
 
-st.write('Polarity:', round(doc._.sentiment.polarity, 2))
-st.write('Subjectivity:', round(doc._.sentiment.subjectivity, 2)) 
+col1, col2, col3 = st.beta_columns(3)
+with col1:
+    word_1 = st.text_input('word 1', 'shirt')
+with col2:
+    word_2 = st.text_input('word 2', 'jeans')
+with col3:
+    word_3 = st.text_input('word 3', 'apple')
+
+nlp = spacy.load("en_core_web_md")
+tokens = nlp(f"{word_1} {word_2} {word_3}")
+
+# get combination of tokens
+comb = combinations(tokens, 2)
+
+most_similar = 0
+match_tokens = None
+
+for token in list(comb):
+    similarity = token[0].similarity(token[1])
+    
+    if similarity > most_similar:
+        most_similar = similarity
+        match_tokens = token
+
+st.write(f'{match_tokens[0]} and {match_tokens[1]} are the most similar with a similarity of {round(most_similar*100, 2)}%')
+
+st.text('https://towardsdatascience.com/streamlit-and-spacy-create-an-app-to-predict-sentiment-and-word-similarities-with-minimal-domain-14085085a5d4')
